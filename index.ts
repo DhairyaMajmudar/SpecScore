826b367df7980c6e4b1338689216b90213801749
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-06-20 21:32:13 +0530

    Add cli interface

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { program } from 'commander';
+
+program
+  .command('score-api')
+  .description('CLI to validate OpenAPI schemas and generate reports')
+  .version('0.1.0');
+
+program
+  .command('validate')
+  .description('Validate an OpenAPI schema file or url link')
+  .argument('<file>', 'Path to the OpenAPI schema file (yaml or json) or URL')
+  .action(() => {});
